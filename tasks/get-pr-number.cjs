324ee242814eec6319fc42bea8e94b60557931d8
Tim Schaub <tim.schaub@gmail.com>
2021-11-15 12:19:48 -0700

    Store results and use later

--- tasks/get-pr-number.cjs
+++ tasks/get-pr-number.cjs
@@ -1,15 +1,12 @@
 /* eslint-disable import/no-unresolved */
 const unzipper = require('unzipper');
-const request = require('request');
 
 module.exports = async ({github, context}) => {
-  const artifacts = github.rest.actions.listWorkflowRunArtifacts({
+  const artifacts = await github.rest.actions.listWorkflowRunArtifacts({
     owner: context.repo.owner,
     repo: context.repo.repo,
     run_id: github.event.workflow_run.id
   });
-
-  console.log('artifacts', artifacts); /* eslint-disable-line no-console */
 
   const artifact = artifacts.data.artifacts.filter(
     artifact => artifact.name === 'pr'
@@ -19,13 +16,44 @@ module.exports = async ({github, context}) => {
     throw new Error('No pr artifact found');
   }
 
-  const directory = await unzipper.Open.url(
-    request,
-    artifact['archive_download_url']
-  );
+  const download = await github.rest.actions.downloadArtifact({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    artifact_id: artifact.id,
+    archive_format: 'zip'
+  });
+
+  const directory = await unzipper.Open.buffer(Buffer.from(download.data));
   const file = directory.files.find(d => d.path === 'number');
   const content = await file.buffer();
   const number = parseInt(content.toString(), 10);
+  const issueNumber = 1;
 
-  console.log('pr number', number); /* eslint-disable-line no-console */
+  const start = ':package:';
+  const author = 'github-actions[bot]';
+
+  const comments = await github.rest.issues.listComments({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: issueNumber
+  });
+
+  const commentExists = comments.data.some(
+    comment => comment.user.login === author && comment.body.startsWith(start)
+  );
+
+  if (!commentExists) {
+    const body = [
+      `${start} Preview the [examples](https://example.com/${number}/examples/) and`,
+      `[docs](https://example.com/${number}/apidoc/) from this branch`,
+      `here: https://example.com/${number}.`
+    ].join(' ');
+
+    await github.rest.issues.createComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: issueNumber,
+      body: body
+    });
+  }
 };
